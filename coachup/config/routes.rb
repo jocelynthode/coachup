726531ef6392b1425e0c4be01eedf4bfad13c2b8
Michael Jungo <michael.jungo@unifr.ch>
2015-10-07 22:27:42 +0200

    change route for devise

--- coachup/config/routes.rb
+++ coachup/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { sessions: 'users/sessions' }
+  devise_for :users, controllers: { sessions: 'users/sessions' },
+    path: "/", path_names: { sign_in: 'login', sign_up: 'register' }
   get 'welcome/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
