5e49e9d01f32ed15eb1037d98fa0ffe1d021143f
amanda_karavolia <amand4msk@gmail.com>
2015-10-10 23:37:46 +0300

    added main layout and login/sign up/profile templates

--- coachup/db/schema.rb
+++ coachup/db/schema.rb
@@ -11,7 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20151007212605) do
 
   # These are extensions that must be enabled in order to support this database
@@ -75,5 +74,4 @@ ActiveRecord::Schema.define(version: 20151007212605) do
   add_foreign_key "subscriptions", "courses"
   add_foreign_key "subscriptions", "users"
   add_foreign_key "training_sessions", "courses"
-
 end
