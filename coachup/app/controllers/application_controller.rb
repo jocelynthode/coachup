a391cc72acfac5fab8a7aaff1667826ea4014ea7
Michael Jungo <michael.jungo@unifr.ch>
2015-10-15 12:13:41 +0200

    add before_action for authentication

--- coachup/app/controllers/application_controller.rb
+++ coachup/app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
