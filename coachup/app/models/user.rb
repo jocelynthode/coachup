e6de62c514bac05269bc5eda5fddb76fcff65a29
Michael Jungo <michael.jungo@unifr.ch>
2015-10-21 18:23:16 +0200

    change to custom validations

--- coachup/app/models/user.rb
+++ coachup/app/models/user.rb
@@ -1,13 +1,13 @@
 class User < ActiveRecord::Base
   # Virtual attribute for authenticating with either username or email
   attr_accessor :login, :username, :email, :realname, :publicvisible
-  validates_presence_of :username
+  validates :real_name, presence: true
+  validates :username, presence: true, length: {within: 3..50}, uniqueness: true
+  validates :email, format: {with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i}
+  validates :password, length: {within: 3..50}
+  validates_presence_of :password_confirmation
+  validates_confirmation_of :password
 
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable,
-         :authentication_keys => [ :login ]
   has_many :taught_courses, class_name: "Course"
   has_many :subscriptions
   has_many :courses, through: :subscriptions
