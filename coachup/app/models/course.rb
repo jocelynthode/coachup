6a42e0a06efa9f1b287178eb6709bc635e471513
Jocelyn Thode <jocelyn.thode@gmail.com>
2015-10-22 11:38:14 +0200

    start work on editing courses

--- coachup/app/models/course.rb
+++ coachup/app/models/course.rb
@@ -11,4 +11,24 @@ class Course < ActiveRecord::Base
   def self.url
     'http://diufvm31.unifr.ch:8090/CyberCoachServer/resources/'
   end
+  # maybe cache that like in a singleton
+  def self.get_all_sports
+    begin
+      response = RestClient::Request.execute(method: :get,
+                                             url: self.url+'sports/',
+                                             headers: { accept: :json })
+      if response.code == 200
+        answer = JSON.parse(response, symbolize_names: true)
+        sports = []
+        answer[:sports].each do |sport|
+          sports << sport[:name]
+        end
+        sports
+      else
+        nil
+      end
+    rescue
+      nil
+    end
+  end
 end
