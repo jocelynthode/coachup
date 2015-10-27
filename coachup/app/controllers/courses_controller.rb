6ab1938ba40b0e4482bc7038dd30a8aabd9cafc8
Jocelyn Thode <jocelyn.thode@gmail.com>
2015-10-27 15:14:37 +0100

    finish work on editing courses

--- coachup/app/controllers/courses_controller.rb
+++ coachup/app/controllers/courses_controller.rb
@@ -31,6 +31,10 @@ class CoursesController < ApplicationController
   end
 
   def update
+=begin
+    TODO find the location and update it using the params
+    I think you'll have to create a locations_params
+=end
     @course = Course.find(params[:id])
 
     if @course.update(course_params)
