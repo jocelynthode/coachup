0637f8aebf706ac9fdda607ca2c134a34c684443
amanda <amand4msk@gmail.com>
2015-10-29 23:17:07 +0100

    delete locations controller and model

--- coachup/db/schema.rb
+++ coachup/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151029125607) do
+ActiveRecord::Schema.define(version: 20151029221103) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,17 +31,6 @@ ActiveRecord::Schema.define(version: 20151029125607) do
   end
 
   add_index "courses", ["coach_id"], name: "index_courses_on_coach_id", using: :btree
-
-  create_table "locations", force: :cascade do |t|
-    t.float    "latitude"
-    t.float    "longitude"
-    t.string   "address"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer  "course_id"
-  end
-
-  add_index "locations", ["course_id"], name: "index_locations_on_course_id", using: :btree
 
   create_table "subscriptions", force: :cascade do |t|
     t.integer  "course_id"
@@ -98,7 +87,6 @@ ActiveRecord::Schema.define(version: 20151029125607) do
   add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
 
   add_foreign_key "courses", "users", column: "coach_id"
-  add_foreign_key "locations", "courses"
   add_foreign_key "subscriptions", "courses"
   add_foreign_key "subscriptions", "users"
   add_foreign_key "training_sessions", "courses"
