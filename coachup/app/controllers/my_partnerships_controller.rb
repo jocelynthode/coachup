7e5ebce76956a5b8d37eac2b4ecc74bee7da7270
Simon Brulhart <simon@brulhart.me>
2015-11-05 13:20:02 +0100

    partnerships: fix wrong href on links to users in the index view

--- coachup/app/controllers/my_partnerships_controller.rb
+++ coachup/app/controllers/my_partnerships_controller.rb
@@ -1,7 +1,10 @@
 class MyPartnershipsController < ApplicationController
 
   def index
-    @partnerships = current_user_partnerships.find()
+    partnerships = current_user_partnerships.find()
+    @partnerships = partnerships.map do |ps|
+      ps.merge :user_id => User.find_by(username: ps[:user])
+    end
   end
 
   def create
