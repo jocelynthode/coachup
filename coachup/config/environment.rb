10e72f386548d99ece7fb2c76f854d16c7eb91c1
Simon Brulhart <simon@brulhart.me>
2015-11-10 10:18:13 +0100

    configure ActionMailer and use 'dotenv' gem to store credentials in dev env

--- coachup/config/environment.rb
+++ coachup/config/environment.rb
@@ -3,3 +3,14 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Mail configuration
+ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SMTP_USERNAME'],
+    :password       => ENV['SMTP_PASSWORD'],
+    :domain         => 'coachup.heroku.com',
+    :enable_starttls_auto => true
+}
