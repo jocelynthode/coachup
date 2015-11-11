b05518ac4b1d9a40ff4dd640d8b3a4f3a50dd12d
Jocelyn Thode <jocelyn.thode@gmail.com>
2015-11-11 11:32:20 +0100

    remove training_session model

--- coachup/db/migrate/20151106183424_modify_columns_from_training_sessions.rb
+++
@@ -1,7 +0,0 @@
-class ModifyColumnsFromTrainingSessions < ActiveRecord::Migration
-  def change
-    remove_column :training_sessions, :lat
-    remove_column :training_sessions, :lng
-    add_column :training_sessions, :schedule, :string
-  end
-end
