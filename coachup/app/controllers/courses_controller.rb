f8896d6599d7c28b152673c4fa38da4117a7af19
Jocelyn Thode <jocelyn.thode@gmail.com>
2015-11-11 10:56:30 +0100

    finish search bar, and reverted the search of users as we may rework the whole view

--- coachup/app/controllers/courses_controller.rb
+++ coachup/app/controllers/courses_controller.rb
@@ -2,11 +2,12 @@ class CoursesController < ApplicationController
   skip_before_action :require_login, only: [:index, :show]
 
   def index
-    @search = Course.search(params[:q])
     if params[:q] == nil then
       @courses = Course.all
     else
-      @courses = @search.result
+      @courses = Course.search(params[:q]).result
+      #Do we want to check for users too ? if so we'll have to move the search result to a different page
+      #@users = User.search(username_or_name_or_last_name_cont: params[:q]).result
     end
   end
 
