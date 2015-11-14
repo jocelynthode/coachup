af5e718a1f6ec1ae14f12b2b1f9402e185a3022d
Michael Jungo <michael.jungo@unifr.ch>
2015-11-14 13:07:40 +0100

    require email for google omniauth

--- coachup/config/initializers/omniauth.rb
+++ coachup/config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV['GOOGLE_CLIENT'], ENV['GOOGLE_SECRET'], {
-    :scope => 'calendar'
+    :scope => 'email,calendar'
   }
 end
