9f62079e280d9ef14dc238ff98b26768d83305b7
Jocelyn Thode <jocelyn.thode@gmail.com>
2015-11-16 20:25:48 +0100

    remove migration drop binaries

--- coachup/db/schema.rb
+++ coachup/db/schema.rb
@@ -53,35 +53,19 @@ ActiveRecord::Schema.define(version: 20151113152601) do
   add_index "subscriptions", ["user_id"], name: "index_subscriptions_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.string   "username"
-    t.string   "name"
-    t.string   "last_name"
-    t.string   "address"
-    t.string   "country"
-    t.string   "phone"
-    t.date     "date_of_birth"
-    t.string   "trophies"
-    t.string   "personal_records"
-    t.string   "education"
-    t.string   "bio"
-    t.string   "aboutme"
-    t.string   "avatar"
+    t.string "email",         default: "", null: false
+    t.string "username"
+    t.string "address"
+    t.string "country"
+    t.string "phone"
+    t.date   "date_of_birth"
+    t.string "education"
+    t.text   "bio"
+    t.string "aboutme"
+    t.string "avatar"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
 
   add_foreign_key "courses", "locations"
