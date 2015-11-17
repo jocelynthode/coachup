5cae83d722411b8b95c5c62e8cde45a5f2ba3cb0
Michael Jungo <michael.jungo@unifr.ch>
2015-11-17 10:18:03 +0100

    fix email uniqueness

--- coachup/db/schema.rb
+++ coachup/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151116201220) do
+ActiveRecord::Schema.define(version: 20151117091527) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,7 +67,6 @@ ActiveRecord::Schema.define(version: 20151116201220) do
     t.datetime "updated_at",                 null: false
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
 
   add_foreign_key "courses", "locations"
