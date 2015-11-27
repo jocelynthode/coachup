8cd862b63080c87cee58fd24e6b4451d1697d48d
Simon Brulhart <simon@brulhart.me>
2015-11-27 14:37:54 +0100

    add facebook omniauth provider

--- coachup/config/initializers/omniauth.rb
+++ coachup/config/initializers/omniauth.rb
@@ -2,4 +2,5 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV['GOOGLE_CLIENT'], ENV['GOOGLE_SECRET'], {
     :scope => 'email,calendar'
   }
+  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
 end
--- coachup/db/schema.rb
+++ coachup/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151123194330) do
+ActiveRecord::Schema.define(version: 20151123224619) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -79,6 +79,7 @@ ActiveRecord::Schema.define(version: 20151123194330) do
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
     t.string   "password",                   null: false
+    t.string   "facebook_uid"
   end
 
   add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
