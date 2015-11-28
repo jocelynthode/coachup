b27a915093b512ea6f4405d7ad837c9f752183dd
liechtia <andrea.liechti@unifr.ch>
2015-11-28 21:35:25 +0100

    removed training sessions from schema again

--- coachup/db/schema.rb
+++ coachup/db/schema.rb
@@ -52,19 +52,7 @@ ActiveRecord::Schema.define(version: 20151123224619) do
 
   add_index "subscriptions", ["course_id"], name: "index_subscriptions_on_course_id", using: :btree
   add_index "subscriptions", ["user_id"], name: "index_subscriptions_on_user_id", using: :btree
-
-  create_table "training_sessions", force: :cascade do |t|
-    t.string   "description"
-    t.datetime "starts_at"
-    t.datetime "ends_at"
-    t.integer  "course_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.string   "schedule"
-  end
-
-  add_index "training_sessions", ["course_id"], name: "index_training_sessions_on_course_id", using: :btree
-
+  
   create_table "users", force: :cascade do |t|
     t.string   "email",         default: "", null: false
     t.string   "username"
