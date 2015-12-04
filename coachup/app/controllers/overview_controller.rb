3725fc7e3d071edf70ea697853c5b748176192db
Simon Brulhart <simon@brulhart.me>
2015-12-04 10:21:24 +0100

    redirect from root to the list of courses when user is signed in
    solves #90

--- coachup/app/controllers/overview_controller.rb
+++ coachup/app/controllers/overview_controller.rb
@@ -1,8 +1,16 @@
 class OverviewController < ApplicationController
 
-  skip_before_action :require_login, only: [:welcome]
+  skip_before_action :require_login
 
   def welcome
   end
 
+  def root
+    if user_signed_in?
+      redirect_to courses_path
+    else
+      # We use render here so the user will be redirected to root and not welcome after he logs in
+      render 'welcome'
+    end
+  end
 end
