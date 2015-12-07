b88a6bc45af19f801ce118ded37d88ae460adbcc
Simon Brulhart <simon@brulhart.me>
2015-12-07 17:19:22 +0100

    use user card partial for partnership index view

--- coachup/app/controllers/my_partnerships_controller.rb
+++ coachup/app/controllers/my_partnerships_controller.rb
@@ -2,9 +2,9 @@ class MyPartnershipsController < ApplicationController
 
   def index
     partnerships = current_user_partnerships.find()
-    @partnerships = partnerships.map do |ps|
-      ps.merge :user_id => User.find_by(username: ps[:user])
-    end
+    @partners = partnerships.map{ |ps|
+      User.find_by(username: ps[:user])
+    }.compact
   end
 
   def create
