d16dffe4def2bf3f33672a49e40f778d8467af11
Michael Jungo <michael.jungo@unifr.ch>
2015-12-10 17:47:03 +0100

    fix jwt error

--- coachup/config/initializers/omniauth.rb
+++ coachup/config/initializers/omniauth.rb
@@ -1,6 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV['GOOGLE_CLIENT'], ENV['GOOGLE_SECRET'], {
-    :scope => 'email,calendar'
+    scope:  'email,calendar',
+    skip_jwt: true
   }
   provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
 end
